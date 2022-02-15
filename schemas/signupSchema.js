9da0a07c70ce4a9990d2d7945d4601c45b55d9da
Liaess <yann.melo@gmail.com>
2022-02-15 10:48:04 -0300

    Preparando revisão de código

--- schemas/signupSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const signupSchema = joi.object({
-  name: joi.string().required(),
-  email: joi.string().required(),
-  password: joi.string().required(),
-});
-
-export default signupSchema;
